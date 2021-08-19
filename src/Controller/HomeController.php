30638d3a855505b047f1ec214e13f6cabab43ea8
adrien-dubois <mcsnoos@gmail.com>
2021-08-20 01:00:26 +0200

    Add TvShows list & WIP single page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TvShowRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController{
+
+    
+    /**
+     * Methods displaying homepage with the third last series
+     *
+     * @Route("/", name="home")
+     * 
+     * @param TvShowRepository $tvShowRepository
+     * @return Response
+     */
+    public function index(TvShowRepository $tvShowRepository):Response
+    {
+        // Get the three last entries in the DB with findBy()
+        $shows = $tvShowRepository->findBy([],
+            ['createdAt'=>"DESC"],
+            3,0
+        );
+
+        // dump($shows);
+
+        return $this->render('home/home.html.twig',[
+            'shows'=>$shows,
+        ]);
+
+    }
+}
--- src/Controller/TvShowController.php
+++ src/Controller/TvShowController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TvShowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class TvShowController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/tvshow/", name="show_list")
      */
-    public function index(): Response
+    public function index(TvShowRepository $tvShowRepository): Response
     {
-        return $this->render('tv_show/index.html.twig', [
-            
+
+        // Get all the series from DB
+        $tvshows = $tvShowRepository->findAll();
+
+        return $this->render('tv_show/list.html.twig', [
+            'tvshows' => $tvshows,
+        ]);
+    }
+
+    /**
+     * Methods which display a serie by his ID
+     *
+     * @Route("/tvshow/{id}", name="show_single", requirements={"id"="\d+"})
+     * 
+     * @param [int] $id
+     * @param TvShowRepository $tvShowRepository
+     * @return Response
+     */
+    public function singleShow($id, TvShowRepository $tvShowRepository): Response
+    {
+        // Get the right serie with the asked ID
+        $show = $tvShowRepository->find($id);
+
+        // If someone ask an ID doesn't exists, send a 404
+        if(!$show){
+            throw $this->createNotFoundException('La série demandée n\'existe pas');
+        }
+
+        return $this->render('tv_show/single.html.twig',[
+            'show'=>$show,
         ]);
     }
 }
