82c09b6c882f3859d16e90f5143af5526029bb1e
adrien-dubois <mcsnoos@gmail.com>
2021-08-24 10:39:25 +0200

    add some fixtures

---
+++ src/Controller/CharacterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CharacterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CharacterController extends AbstractController
+{
+    /**
+     * @Route("/character", name="character")
+     */
+    public function index(CharacterRepository $characterRepository): Response
+    {
+        return $this->render('character/index.html.twig', [
+            'characters' => $characterRepository->findAll(),
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,7 +14,13 @@ class AppFixtures extends Fixture
     {
 
         $faker = \Faker\Factory::create();
+        $fakers = \Faker\Factory::create();
         $faker->addProvider(new \Xylis\FakerCinema\Provider\TvShow($faker));
+        $fakers->addProvider(new \Xylis\FakerCinema\Provider\Character($fakers));
+        $fake = \Faker\Factory::create();
+        $fake->addProvider(new \Xylis\FakerCinema\Provider\Person($fake));
+
+
         
         for ($i=0; $i <5 ; $i++) { 
 
@@ -23,8 +29,18 @@ class AppFixtures extends Fixture
             $tvShow->setSynopsis($faker->overview);
             $tvShow->setNbLikes(15);
             $tvShow->setImage('01.png');
-            $tvShow->setCreatedAt(new DateTimeImmutable());
 
+            $gender = mt_rand(0, 1) ? 'male' : 'female';
+            $fullNameArray = explode(" ", $fakers->character($gender));
+
+            // On créé un personnage vide
+            $character = new Character();
+            $character->setFirstname($fullNameArray[0]);
+            $character->setLastname($fullNameArray[1] ?? ' Doe' . $i);
+            $character->setGender($gender == 'male' ? 'Homme' : 'Femme');
+            $character->setTruename($fake->actor);
+
+            $manager->persist($character);
             $manager->persist($tvShow);
 
         }
