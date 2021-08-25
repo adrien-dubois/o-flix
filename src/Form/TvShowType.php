933a227112f62db303268e545f636ab7b9f36e4e
adrien-dubois <mcsnoos@gmail.com>
2021-08-25 16:40:19 +0200

    Add TvShow CRUD

---
+++ src/Form/TvShowType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\TvShow;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TvShowType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('synopsis')
+            ->add('image')
+            ->add('nbLikes')
+            ->add('publishedAt')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('characters')
+            ->add('categories')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => TvShow::class,
+        ]);
+    }
+}
